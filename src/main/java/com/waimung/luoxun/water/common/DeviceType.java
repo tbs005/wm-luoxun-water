6f27305953dde46a14d56c1f67eeef060a506a8b
w <w@PC-20170712HVTB>
2020-08-11 17:52:10 +0800

    添加设备类型

--- src/main/java/com/waimung/luoxun/water/common/DeviceType.java
+++ src/main/java/com/waimung/luoxun/water/common/DeviceType.java
@@ -1,6 +1,14 @@
 package com.waimung.luoxun.water.common;
 
-public class DeviceType {
+public enum DeviceType {
 
+	WL(198,"水位传感器"),WP(199,"水压传感器");
+
+	private int type;
+	private String remark;
 	
+	private DeviceType(int type, String remark) {
+		this.type = type;
+		this.remark = remark;
+	}
 }
